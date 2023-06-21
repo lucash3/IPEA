73adf4fff362491c3e6eaa97c09fca9f54b38348
Lucas Henrique <lucashpsn@gmail.com>
2023-06-21 00:32:05 -0300

    Update script

--- dirur/scripts/tratamento_LOA.R
+++ dirur/scripts/tratamento_LOA.R
@@ -29,6 +29,8 @@ SIGA_2021 <- fread("SIGA 2021.csv", sep = ";", dec = ",")
 SIGA_2022 <- fread("SIGA 2022.csv", sep = ";", dec = ",")
 
 
+
+
 # Cria vetor de nomes
 new_names <- c("ANO", "LOCALIDADE_UF", "REGIAO", "UF*", "REGIAO*", "UF*_NOVO",
                "ORGAO_COD_DESC", "ORGAO_SUPERIOR_COD_DESC", "UG_COD_DESC", "UG_UF", "UO_COD_DESC_AJUSTADO",
@@ -37,7 +39,9 @@ new_names <- c("ANO", "LOCALIDADE_UF", "REGIAO", "UF*", "REGIAO*", "UF*_NOVO",
                "EMPENHADO", "DESPESA_EXECUTADA", "EMPENHADO_IGPDI", "DESPESA_EXECUTADA_IGPDI")
 
 # Lista
-dataframes <- list(LOA_2001, LOA_2002, LOA_2003, LOA_2004, LOA_2005, LOA_2006, LOA_2007, LOA_2008, LOA_2009, LOA_2010, LOA_2011, LOA_2012, LOA_2013, LOA_2014, LOA_2015, LOA_2016, LOA_2017, LOA_2018, LOA_2019)
+dataframes <- list(LOA_2000, LOA_2001, LOA_2002, LOA_2003, LOA_2004, LOA_2005, LOA_2006,
+                   LOA_2007, LOA_2008, LOA_2009, LOA_2010, LOA_2011, LOA_2012, LOA_2013,
+                   LOA_2014, LOA_2015, LOA_2016, LOA_2017, LOA_2018, LOA_2019, LOA_2020)
 
 # Renomeia para cada data frame
 dataframes <- lapply(dataframes, function(df) {
@@ -47,24 +51,28 @@ dataframes <- lapply(dataframes, function(df) {
 
 
 
-# Atualiza os nomes dos dataframes 
-LOA_2001 <- dataframes[[1]]
-LOA_2002 <- dataframes[[2]]
-LOA_2003 <- dataframes[[3]]
-LOA_2004 <- dataframes[[4]]
-LOA_2005 <- dataframes[[5]]
-LOA_2006 <- dataframes[[6]]
-LOA_2007 <- dataframes[[7]]
-LOA_2008 <- dataframes[[8]]
-LOA_2009 <- dataframes[[9]]
-LOA_2010 <- dataframes[[10]]
-LOA_2011 <- dataframes[[11]]
-LOA_2012 <- dataframes[[12]]
-LOA_2013 <- dataframes[[13]]
-LOA_2014 <- dataframes[[14]]
-LOA_2015 <- dataframes[[15]]
-LOA_2016 <- dataframes[[16]]
-LOA_2017 <- dataframes[[17]]
-LOA_2018 <- dataframes[[18]]
-LOA_2019 <- dataframes[[19]]
-LOA_2020 <- dataframes[[20]]
+# Atualiza os nomes dos dataframes
+LOA_2000 <- dataframes[[1]]
+LOA_2001 <- dataframes[[2]]
+LOA_2002 <- dataframes[[3]]
+LOA_2003 <- dataframes[[4]]
+LOA_2004 <- dataframes[[5]]
+LOA_2005 <- dataframes[[6]]
+LOA_2006 <- dataframes[[7]]
+LOA_2007 <- dataframes[[8]]
+LOA_2008 <- dataframes[[9]]
+LOA_2009 <- dataframes[[10]]
+LOA_2010 <- dataframes[[11]]
+LOA_2011 <- dataframes[[12]]
+LOA_2012 <- dataframes[[13]]
+LOA_2013 <- dataframes[[14]]
+LOA_2014 <- dataframes[[15]]
+LOA_2015 <- dataframes[[16]]
+LOA_2016 <- dataframes[[17]]
+LOA_2017 <- dataframes[[18]]
+LOA_2018 <- dataframes[[19]]
+LOA_2019 <- dataframes[[20]]
+LOA_2020 <- dataframes[[21]]
+
+
+LOA_FINAL <- do.call(rbind, lapply(paste0("LOA_", 2001:2020), function(x) get(x)))
